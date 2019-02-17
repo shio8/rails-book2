cdcad045b3e4ae932917d19ff84d88523e44d74d
shio8 <kaihatu.shio@gmail.com>
2019-02-17 19:41:29 +0900

    chapter5 finished

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,15 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_conma
+
+  belongs_to :user
+
+  scope :recent, -> { order(created_at: :desc)}
+
+  private
+
+  def validate_name_not_including_conma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
